426e282a5f1d82478cb2b6cc93f4e9a8c5820a73
pavlosts <pavlos_tsaou@hotmail.gr>
2014-12-09 20:13:15 +0200

    Output to file
    Port scanner now outputs results both in screen and in file.

--- Port Scanner/portsc.py
+++ Port Scanner/portsc.py
@@ -2,6 +2,8 @@ import socket
 import sys
 import time
 
+file = open("portscan.txt", "w")
+
 print("Enter hostname or IP.")
 IP = input()
 
@@ -12,13 +14,18 @@ except socket.gaierror:
     time.sleep(5)
     sys.exit()
 
+text = 'Results from port scanning in target :' + str(IP) + '\n'
+file.write(text)
+
 try:
-    for port in range(1,1025):
+    for port in range(1, 1025):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         res = sock.connect_ex((HOST, port))
         if res is 0:
             print("Port :", port, "\tis open!")
+            text = '\tPort :' + str(port) + "\t is open!\n"
+            file.write(text)
 
         sock.close()
 
@@ -27,3 +34,4 @@ except socket.error:
     time.sleep(5)
     sys.exit()
 
+file.close()
