c1278d8605572ecc412a8a6eb6410e0c649540ff
pavlosts <pavlos_tsaou@hotmail.gr>
2015-01-10 23:00:24 +0200

    Speed improved
    Made it 200x faster!

--- portsc.py
+++ portsc.py
@@ -3,15 +3,29 @@ import sys
 import time
 import datetime
 import findport
+import threading
 
 cur_date = datetime.datetime.now()
 
-PORT_START = 79
-PORT_END = 81
+PORT_START = 1
+PORT_END = 1400
+
+
+def check_port(host, port):
+    text = ""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    res = sock.connect_ex((host, port))  # Tries to connect to the specific port
+    if res is 0:  # If the value returned is 0 then the port is open
+        print("Port :", port, "\tis open!")
+        text = '\tPort :' + str(port) + "\t is open!\n"
+        findport.find_port(port)
+
+    sock.close()
+    return text
 
 
 def portscan():
-    start = time.time()                                                                          # Time counter for scan
+    start = time.time()  # Time counter for scan
 
     ip = input("Enter target's IP or hostname\n")
 
@@ -23,22 +37,21 @@ def portscan():
         sys.exit()
 
     filename = 'portscan.txt'
-    file = open(filename, "w")                                   # Opens file portscan.txt where results will be printed
+    file = open(filename, "w")  # Opens file portscan.txt where results will be printed
 
     text = 'Results from port scanning in target : ' + str(ip) + '\nDate : ' + str(cur_date) + '\n'
     file.write(text)
 
     try:
-        for port in range(PORT_START, PORT_END):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            res = sock.connect_ex((host, port))                                  # Tries to connect to the specific port
-            if res is 0:                                              # If the value returned is 0 then the port is open
-                print("Port :", port, "\tis open!")
-                text = '\tPort :' + str(port) + "\t is open!\n"
-                file.write(text)
-                findport.find_port(port)
-
-            sock.close()
+        port = PORT_START
+        while port <= PORT_END:
+            for i in range(1, 5):
+                t = threading.Thread(target=check_port, args=(host, port, ))
+                t.start()
+                if text != "":
+                    file.write(text)
+                port += 1
+                time.sleep(0.001)
 
     except socket.error:
         print("Could not connect to ", ip)
@@ -47,7 +60,7 @@ def portscan():
 
     file.close()
 
-    end = time.time()                                                                              # End of time counter
+    end = time.time()  # End of time counter
 
     print("Scan finished in ", str(end - start), 's.')
     input("Press ENTER to exit")
